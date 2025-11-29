05146464404b082079ea3f0c77829c678672f125
Aaditya Shukla <itsaadityashukla@gmail.com>
2025-11-29 21:11:22 +0530

    Added submission answer model and controller tests

--- test/models/submission_answer_test.rb
+++ test/models/submission_answer_test.rb
@@ -3,7 +3,34 @@
 require "test_helper"
 
 class SubmissionAnswerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @submission_answer = build(:submission_answer)
+  end
+
+  def test_valid_submission_answer
+    assert @submission_answer.valid?
+  end
+
+  def test_submission_presence
+    @submission_answer.submission = nil
+    assert_not @submission_answer.valid?
+    assert_includes @submission_answer.errors[:submission], "must exist"
+  end
+
+  def test_question_presence
+    @submission_answer.question = nil
+    assert_not @submission_answer.valid?
+    assert_includes @submission_answer.errors[:question], "must exist"
+  end
+
+  def test_selected_option_presence
+    @submission_answer.selected_option = nil
+    assert_not @submission_answer.valid?
+    assert_includes @submission_answer.errors[:selected_option], "must exist"
+  end
+
+  def test_is_correct_boolean
+    @submission_answer.is_correct = nil
+    assert_not @submission_answer.valid?
+  end
 end
