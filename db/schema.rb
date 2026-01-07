d4d22d887d6f4e9c23fc768d063db0dcf594d3e8
Aaditya Shukla <itsaadityashukla@gmail.com>
2026-01-07 21:10:50 +0530

    Changed the validations of options to unique

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_23_210351) do
+ActiveRecord::Schema[7.1].define(version: 2026_01_07_144402) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_23_210351) do
     t.boolean "is_correct"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["question_id", "content"], name: "index_options_on_question_id_and_content", unique: true
   end
 
   create_table "organizations", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
